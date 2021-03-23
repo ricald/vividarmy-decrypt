2938e8e556bd96c89472104494b0c7de300d6b1c
Ricald <ricald21+git@gmail.com>
2021-03-23 20:49:32 +0000

    新規作成

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace vividarmy_decrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sr = new StreamReader("data.txt"))
+            {
+                while (true)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    var answer = string.Empty;
+                    foreach (var c in line)
+                    {
+                        answer += (char)(c - 3);
+                    }
+
+                    Console.WriteLine(answer);
+                }
+            }
+        }
+    }
+}
